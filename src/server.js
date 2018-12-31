82f9ed280d3648e4bdb69614b234ae0fe771cb20
Henry <comprocho@gmail.com>
2018-12-31 00:31:48 +0000

    wip

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
 
+
 export const app = express()
+
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +15,42 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+router.get('/me', (req, res) => {
+    res.send({me: 'hello'})
+})
+
+const routes = ['get /cat', 'get cat/:id', 'post /cat', 'put /cat/:id', 'delete /cat:/id']
+
+router.route('/cat')
+    .get()
+    .post()
+
+router.route('/cat/:id')
+    .get()
+    .put()
+    .delete()
+
+app.use('/api', router)
+// CRUD
+app.get('/', (req, res, next) => {
+    // res.send({ data: 1})
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send({ data: 2})
+})
+
+
+app.post('/data', (req, res) => {
+    console.log(req.body)
+    res.send({ ok: true})
+})
+
+const port = process.env.PORT
+
+export const start = () => {
+  app.listen(port, () => {
+      console.log(`server on ${port}`)
+  })  
+}
